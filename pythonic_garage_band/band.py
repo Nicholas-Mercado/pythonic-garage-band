8874dd0ba663cbc76e9452398986fa63f3ecc96c
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-04-21 15:33:49 -0700

    seven test passed

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,37 +1,50 @@
 
-class Band():
-    pass
-
-class Musician():
-    pass
-
-class Guitarist():
     
-    def __init__(self, name='unknown'):
+class Band:
+    
+    def __init__(self, name="unknown", members="unknown"):
         self.name = name
-        
+        self.members = members  
+    
+
+class Musician:
+    def __init__(self, name="unknown"):
+        self.name = name
+
+
+
+class Guitarist(Musician):
+    def __init__(self, name="unknown"):
+        self.name = name
+
     def __repr__(self):
-        return f"Guitarist instance. Name = {self.name}"    
+        return f"Guitarist instance. Name = {self.name}"
 
     def __str__(self):
         return f"My name is {self.name} and I play guitar"
-    
 
-class Bassist():
-    pass
 
-class Drummer():
+class Bassist(Musician):
     
-    def __init__(self, name='unknown'):
+    def __init__(self, name="unknown"):
         self.name = name
-        
-    def __str__(self, name='unknown'):
-        return f"My name is {self.name} and I play drums"
     
-    # def test_drummer_str():
-    # sheila = Drummer("Sheila E.")
-    # actual = str(sheila)
-    # expected = "My name is Sheila E. and I play drums"
-    # assert actual == expected
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+    
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
 
 
+class Drummer(Musician):
+    def __init__(self, name="unknown"):
+        self.name = name
+
+    def __str__(self, name="unknown"):
+        return f"My name is {self.name} and I play drums"
+
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
+
+
+nirvana = Band("Nirvana", [])
