1f5c49e727778e406118ca9e19ab6a4909328899
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-04-21 15:43:43 -0700

    ten tests passed

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,16 +1,15 @@
-
-    
 class Band:
-    
     def __init__(self, name="unknown", members="unknown"):
         self.name = name
-        self.members = members  
-    
+        self.members = members
+
 
 class Musician:
     def __init__(self, name="unknown"):
         self.name = name
 
+    def get_instrument(self):
+        return "guitar"
 
 
 class Guitarist(Musician):
@@ -23,17 +22,22 @@ class Guitarist(Musician):
     def __str__(self):
         return f"My name is {self.name} and I play guitar"
 
+    def get_instrument(self):
+        return "guitar"
+
 
 class Bassist(Musician):
-    
     def __init__(self, name="unknown"):
         self.name = name
-    
+
     def __str__(self):
         return f"My name is {self.name} and I play bass"
-    
+
     def __repr__(self):
         return f"Bassist instance. Name = {self.name}"
+
+    def get_instrument(self):
+        return "bass"
 
 
 class Drummer(Musician):
@@ -45,6 +49,9 @@ class Drummer(Musician):
 
     def __repr__(self):
         return f"Drummer instance. Name = {self.name}"
+
+    def get_instrument(self):
+        return "drums"
 
 
 nirvana = Band("Nirvana", [])
