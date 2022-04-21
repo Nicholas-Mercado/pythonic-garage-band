d5d4cf5fbf1bd8aed3863a014f248d77d4a96fcf
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-04-21 14:54:32 -0700

    passed third test

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -6,11 +6,32 @@ class Musician():
     pass
 
 class Guitarist():
-    pass
+    
+    def __init__(self, name='unknown'):
+        self.name = name
+        
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"    
+
+    def __str__(self):
+        return f"My name is {self.name} and I play guitar"
+    
 
 class Bassist():
     pass
 
 class Drummer():
-    pass
+    
+    def __init__(self, name='unknown'):
+        self.name = name
+        
+    def __str__(self, name='unknown'):
+        return f"My name is {self.name} and I play drums"
+    
+    # def test_drummer_str():
+    # sheila = Drummer("Sheila E.")
+    # actual = str(sheila)
+    # expected = "My name is Sheila E. and I play drums"
+    # assert actual == expected
+
 
